874b4bd76b65d996fd7107df5db95d1e6d78548e
Roman Dumanskyi <dumanskyi.r@gmail.com>
2024-03-10 18:15:57 +0200

    .

--- 4_day/run.js
+++ 4_day/run.js
@@ -1,14 +1,44 @@
 import EventEmitter from 'node:events';
+import fs from 'fs';
+import path from 'path';
 
-const ee = new EventEmitter();
 
-console.log('START');
-ee.on('event', () => console.log('EVENT'));
-ee.on('event', () => console.log('EVENT_2'));
-ee.on('log', () => console.log('LOG'));
+// const ee = new EventEmitter();
+//
+// const listenerOnce = () => {
+//   console.log('EVENT ONCE');
+// }
+//
+// console.log('+++++++++++++++++++++++++++++++++START');
+// ee.on('event', () => console.log('EVENT'));
+// ee.on('event', () => console.log('EVENT_2'));
+// ee.once('event', listenerOnce);
+//
+// ee.on('log', (date, category, message) => console.log('LOG', date, category, message));
+// ee.prependListener('event', () => console.log('I am first'));
+// ee.removeListener('event', listenerOnce);
+// ee.removeAllListeners('event');
+//
+//
+//
+// ee.emit('event');
+// ee.emit('event');
+// ee.emit('log', new Date(), 'main.js', 'TEXT')
+//
+// console.log('+++++++++++++++++++++++++++++++++FINISH', ee.eventNames());
+//
+// process.on('beforeExit', () => console.log('I am closing'));
 
-// console.log(ee.eventNames());
 
-ee.emit('event');
+const readStream = fs.createReadStream(path.join('./', 'log.txt'));
 
-console.log('FINISH', ee.eventNames());
+readStream.on('open', () => {
+  console.log('File read STARTED');
+})
+readStream.on('data', (chunk) => {
+  console.log(chunk, 'chunk');
+});
+
+readStream.on('end', () => {
+  console.log('File read finished');
+})
